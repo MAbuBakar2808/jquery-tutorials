bfc72170ae50cb60e7984117741972ef38dee523
MAbuBakar <muhammadabubakar256@gmail.com>
2017-04-11 03:56:26 +0500

    Even Odd Selector

---
+++ even-odd.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    <input type="text">
+    <script  src="jquery.js"></script>
+    <script  src="text-selector.js"></script>
+
+</body>
+</html>
