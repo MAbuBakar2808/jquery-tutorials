0bcd622719449da9a755c47f5f2e3773597ac2a9
MAbuBakar <muhammadabubakar256@gmail.com>
2017-04-11 00:52:51 +0500

    Document Ready handler

---
+++ ready.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    <p>This is a paragraph</p>
+    <script  src="jquery.js"></script>
+    <script  src="ready.js"></script>
+
+</body>
+</html>
