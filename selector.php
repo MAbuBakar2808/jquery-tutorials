d3aff14d33c89f5d3d998220dc9c6a5adcbee42d
MAbuBakar <muhammadabubakar256@gmail.com>
2017-04-11 02:13:14 +0500

    All Selector

---
+++ selector.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    
+    <script  src="jquery.js"></script>
+    <script  src="selector.js"></script>
+
+</body>
+</html>
