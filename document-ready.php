67bf4a81ac057e459f38e2c865a2d2f561897878
MAbuBakar <muhammadabubakar256@gmail.com>
2017-04-11 00:25:00 +0500

    Document ready FadeIn

---
+++ document-ready.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    <p id="paragraph" style="display: none;">This is a paragraph</p>
+    <script  src="jquery.js"></script>
+    <script  src="init.js"></script>
+
+</body>
+</html>
