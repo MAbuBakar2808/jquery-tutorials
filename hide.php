9b225ceeb91818d593577c74628228402fccd022
MAbuBakar <muhammadabubakar256@gmail.com>
2017-04-11 00:05:44 +0500

    Jquer Onclick

---
+++ hide.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    <p id="paragraph">This is a paragraph</p>
+    <script  src="jquery.js"></script>
+    <script  src="hide.js"></script>
+
+</body>
+</html>
