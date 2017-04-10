629c70992787690d0bb97edc8126998811b0ed8e
MAbuBakar <muhammadabubakar256@gmail.com>
2017-04-11 04:13:22 +0500

    Attribute Selector

---
+++ attribute-selector.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+	var email_default = 'Please Enter the Email Address..';
+
+	$('input[type="email"]').attr("value", email_default);
+	
+});
+
---
+++ attribute-selector.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    <p>Name <br><input type="text" ></p>
+    <p>Email <br><input type="email" size="30"></p>
+    <script  src="jquery.js"></script>
+    <script  src="attribute-selector.js"></script>
+
+</body>
+</html>
