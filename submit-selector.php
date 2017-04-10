47f39b4624683284a80333a365332071cbae42f9
MAbuBakar <muhammadabubakar256@gmail.com>
2017-04-11 02:51:22 +0500

    Submit Selector

---
+++ submit-selector.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    <input type="submit" value="Register" >
+    <input type="submit" value="Submit" >
+    <script  src="jquery.js"></script>
+    <script  src="submit-selector.js"></script>
+
+</body>
+</html>
